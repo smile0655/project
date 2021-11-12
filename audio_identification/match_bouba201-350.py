2870e22648abdb9cc6724e661af72c10992b701b
Hongjun Zhou <54527252+smile0655@users.noreply.github.com>
2021-11-12 04:44:27 +0100

    Update match_bouba201-350.py

--- audio_identification/match_bouba201-350.py
+++ audio_identification/match_bouba201-350.py
@@ -41,4 +41,4 @@ for ii in range(1,7):
     print(maxcount)
     print(f'bouba_{identified_file:04d}.wav')
     print(offset)
-    np.savez_compressed('./last_match/boubacounter201-350'+f"{ii}", track_most_common_offset_counter)
+    np.savez_compressed('./last_match/boubacounter201-350'+f"Q{ii}", track_most_common_offset_counter)
